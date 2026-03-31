32fd0ef5c208ee244a7355cbfa096a206c7c6ab6
AbdallahDev <abdallah.dev85@gmail.com>
2026-03-31 17:49:41 +0300

    stage

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello node")
+import http from "node:http"
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+
+    res.write("Data 1\n")
+    res.write("Another data\n")
+
+    if (req.url === "/") {
+        res.end("Hello, From my api ❤️", 'utf-8', () => console.log("responsed has finished."))
+    } else {
+        res.end()
+    }
+})
+
+
+server.listen(PORT, () => console.log(`server running successfully on port ${PORT}`))
